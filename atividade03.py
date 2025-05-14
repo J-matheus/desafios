a32013b35039c878b78c997dc7bf12631e9af58b
jose matheus <j.matheus.v.souza@gmail.com>
2025-05-14 13:01:22 -0300

    divisao de uma soma

---
+++ atividade03.py
@@ -0,0 +1,8 @@
+num1= int(input("informe o primeiro número: "))
+num2= int(input("informe o segundo número: "))
+
+soma= num1+num2
+divisao= soma/2
+
+print(f"O resultado de {num1} + {num2} é {soma} sendo dividido por 2, fica {divisao}.")
+
