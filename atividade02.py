dfb80d676eb9082a3aca446664909c7b5eae035d
jose matheus <j.matheus.v.souza@gmail.com>
2025-05-14 12:29:05 -0300

    login e senha

---
+++ atividade02.py
@@ -0,0 +1,11 @@
+user= "matheus"
+key="1234"
+
+login= input("Informe o login do usuario: ")
+senha= input("Informe o senha do usuario: ")
+
+if login ==user and senha ==key:
+    print("Acesso permitido")
+else:
+    print("Acesso negado "
+          "\nConfira se seu login e senha estão corretos.")
